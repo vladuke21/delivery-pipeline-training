dbab4e2ad3903cf377155762435309cc226959f0
Vladimir V. Koval <v.koval@semrush.com>
2016-04-06 11:43:51 +0300

    changes for newrelic

--- newrelic.js
+++ newrelic.js
@@ -8,11 +8,11 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name: ['pumpkin-tart-71750'],
+  app_name: ['damp-inlet-58780'],
   /**
    * Your New Relic license key.
    */
-  license_key: 'eee61b7f7f91dd27420975c00ae02d8f97bb6922',
+  license_key: 'f443be164b65aef3d74f9abf6f6d9c29e66a1502',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
